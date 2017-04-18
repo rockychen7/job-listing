53ea60086280f182af3c662e5637ef6d573b19ce
陈帅 <rockychen7@gmail.com>
2017-04-18 12:38:44 +0800

    rename hidden

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -9,44 +9,44 @@ class JobsController < ApplicationController
     @jobs = Job.all.where(:is_hidden => false).order("created_at DESC")
   end
 
-  def new
-    @job = Job.new
-  end
+  #def new
+    #@job = Job.new
+  #end
 
-  def create
-    @job = Job.new(job_params)
+  #def create
+  #  @job = Job.new(job_params)
 
-    if @job.save
-      redirect_to jobs_path
-    else
-      render :new
-     end
-    end
-
-
-  def edit
-   @job = Job.find(params[:id])
- end
-
- def update
-   @job = Job.find(params[:id])
-   if @job.update(job_params)
-
-   redirect_to jobs_path
- else
-   render edit
- end
- end
+    #if @job.save
+      #redirect_to jobs_path
+    #else
+    #  render :new
+    # end
+  #  end
 
 
- def destroy
-   @job = Job.find(params[:id])
+  #def edit
+  # @job = Job.find(params[:id])
+ #end
 
-   @job.destroy
+ #def update
+  # @job = Job.find(params[:id])
+  # if @job.update(job_params)
 
-   redirect_to jobs_path
+   #redirect_to jobs_path
+ #else
+   #render edit
+# end
+ #end
 
- end
+
+ #def destroy
+   #@job = Job.find(params[:id])
+
+#   @job.destroy
+
+   #redirect_to jobs_path
+
+# end
 
  private
 
