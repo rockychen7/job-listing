764a8fbabb61c585d3074855cda3cc3fadf121e9
陈帅 <rockychen7@gmail.com>
2017-04-19 11:27:11 +0800

    order scope

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,55 +3,62 @@ class JobsController < ApplicationController
 
   def show
     @job = Job.find(params[:id])
-    
+
         if @job.is_hidden
-       flash[:warning] = "This Job already archieved"
-       redirect_to root_path
+           flash[:warning] = 'This Job already archieved'
+           redirect_to root_path
      end
   end
 
   def index
-    @jobs = Job.all.where(:is_hidden => false).order("created_at DESC")
-  end
+        @jobs = case params[:order]
+               when 'by_lower_bound'
+                Job.published.order('wage_lower_bound DESC')
+               when 'by_upper_bound
+                 Job.published.order('wage_upper_bound DESC')
+               else
+                 Job.published.recent
+               end
+             end
 
-  #def new
-    #@job = Job.new
-  #end
-
-  #def create
-  #  @job = Job.new(job_params)
-
-    #if @job.save
-      #redirect_to jobs_path
-    #else
-    #  render :new
-    # end
-  #  end
+#  def new
+#  @job = Job.new
+#  end
 
 
-  #def edit
-  # @job = Job.find(params[:id])
- #end
+#  def create
+#    @job = Job.new(job_params)
 
- #def update
-  # @job = Job.find(params[:id])
-  # if @job.update(job_params)
+#  if @job.save
+#  redirect_to jobs_path
+#  else
+#    render :new
+#   end
+#    end
 
-   #redirect_to jobs_path
- #else
-   #render edit
-# end
- #end
+#  def edit
+#   @job = Job.find(params[:id])
+#  end
+
+#  def update
+#   @job = Job.find(params[:id])
+#   if @job.update(job_params)
+
+#  redirect_to jobs_path
+#  else
+#  render edit
+#   end
+#  end
 
 
- #def destroy
-   #@job = Job.find(params[:id])
+#  def destroy
+#  @job = Job.find(params[:id])
 
-#   @job.destroy
+#     @job.destroy
 
-   #redirect_to jobs_path
+#  redirect_to jobs_path
 
-# end
+#   end
 
  private
 
