d40d906ad0cfe28819bb6c3a78578dc2b8b7a29c
陈帅 <rockychen7@gmail.com>
2017-04-19 13:19:33 +0800

    submit resume

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -14,7 +14,7 @@ class JobsController < ApplicationController
         @jobs = case params[:order]
                when 'by_lower_bound'
                 Job.published.order('wage_lower_bound DESC')
-               when 'by_upper_bound
+              when 'by_upper_bound'
                  Job.published.order('wage_upper_bound DESC')
                else
                  Job.published.recent
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: jobs
+#
+#  id               :integer          not null, primary key
+#  title            :string
+#  description      :text
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  wage_upper_bound :integer
+#  wage_lower_bound :iteger
+#  contact_email    :string
+#  is_hidden        :boolean          default(TRUE)
+#
+
+
 class Job < ApplicationRecord
   validates :title, presence: true
   validates :wage_upper_bound, presence: true
@@ -7,6 +23,7 @@ class Job < ApplicationRecord
 
   scope :published, -> { where(is_hidden: false) }
   scope :recent, -> { order('created_at DESC') }
+  has_many :resumes
 
 
   def publish!
