6cd55e674aab4e4195c63379b07cd05ed1fb5961
陈帅 <rockychen7@gmail.com>
2017-04-19 14:02:11 +0800

    add attachment upload column

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,17 +1,17 @@
 class ResumesController < ApplicationController
    before_action :authenticate_user!
- 
+
    def new
      @job = Job.find(params[:job_id])
      @resume = Resume.new
    end
- 
+
    def create
      @job = Job.find(params[:job_id])
      @resume = Resume.new(resume_params)
      @resume.job = @job
      @resume.user = current_user
- 
+
      if @resume.save
        flash[:notice] = "成功提交履歷"
        redirect_to job_path(@job)
@@ -19,10 +19,10 @@ class ResumesController < ApplicationController
        render :new
      end
    end
- 
+
    private
- 
+
    def resume_params
-     params.require(:resume).permit(:content)
+     params.require(:resume).permit(:content, :attachment)
    end
  end
